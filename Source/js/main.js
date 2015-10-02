74bd46fd6fbaf75949496fabe80658ae50d3033a
huthaifa-abd <huth2004@gmail.com>
2015-10-02 23:25:00 +0300

    -Adding Template Functions -Adding Custom Summary Template -Code Optimization And XML data support

--- Source/js/main.js
+++ Source/js/main.js
@@ -8,13 +8,66 @@ app.config(function ($mdIconProvider) {
     .defaultIconSet('img/icons/sets/core-icons.svg', 24);
 })
 
-
 app.controller('AppCtrl', function ($scope, $http, $mdSidenav, $mdDialog) {
+  var templateFile = '';
+
+
+  angular.element(document).ready(function () {
+    //Load Tempaltes
+    //Load Summary Templates
+    $http.get('templates/SummaryTemplates.json').success(function (data) {
+      $scope.summaryTemplates = [];
+      angular.forEach(data.Templates, function (value, key) {
+        $scope.summaryTemplates.push(value);
+      });
+    });
+
+  });
+
+$scope.UpdateSelectedSummartTemplate = function (position ,summaryTemplates)
+{
+   angular.forEach(summaryTemplates, function (template, index) {
+      if (position != index) {
+        template.checked = false;
+      }
+      else
+      {
+         template.checked = true;
+      }
+   });
+}
+
+
   $scope.toggleSidenav = function (menuId) {
     $mdSidenav(menuId).toggle();
   };
 
+  $scope.showAdvanced = function (ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'customizesummary.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose: true
+    })
+      .then(function (answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function () {
+        $scope.status = 'You cancelled the dialog.';
+      });
+  };
 
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function () {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function () {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function (answer) {
+      $mdDialog.hide(answer);
+    };
+  };
 
 
 
@@ -38,6 +91,30 @@ app.controller('AppCtrl', function ($scope, $http, $mdSidenav, $mdDialog) {
     { name: 'Resume', checked: false },
     { name: 'Cover Letter', checked: false }
   ];
+
+  $scope.UpdateInformation = function ($infoType) {
+    
+    //Load Data
+    
+    $http.get(templateFile).success(function (data) {
+
+      var resumeTemplate = data;
+      if (typeof $scope.company != 'undefined') {
+
+        if (typeof $scope.company.name != 'undefined')
+          resumeTemplate = resumeTemplate.replace(/--CompanyName--/g, $scope.company.name);
+        if (typeof $scope.company.address != 'undefined')
+          resumeTemplate = resumeTemplate.replace(/--CompanyAddress--/g, $scope.company.address);
+        if (typeof $scope.company.jobtitle != 'undefined')
+          resumeTemplate = resumeTemplate.replace(/--JobTitle--/g, $scope.company.jobtitle);
+
+      }
+      $scope.data = {
+        text: resumeTemplate
+      }
+    });
+  }
+
   $scope.updateSelection = function (position, doctypes) {
     angular.forEach(doctypes, function (doctype, index) {
       if (position != index) {
@@ -45,24 +122,29 @@ app.controller('AppCtrl', function ($scope, $http, $mdSidenav, $mdDialog) {
       }
       else {
         doctype.checked = true;
-        if (doctype.name == 'Resume')
-
-          $http.get('resumetemplate.txt').success(function (data) {
+        if (doctype.name == 'Resume') {
+          templateFile = 'templates/resumetemplate.txt';
+          $http.get('templates/resumetemplate.txt').success(function (data) {
             $scope.data = {
               text: data
             }
+            $scope.UpdateInformation();
           });
-          
-          if (doctype.name == 'Cover Letter')
-
-          $http.get('covertemplate.txt').success(function (data) {
+        }
+        if (doctype.name == 'Cover Letter') {
+          templateFile = 'templates/covertemplate.txt';
+          $http.get('templates/covertemplate.txt').success(function (data) {
             $scope.data = {
               text: data
             }
+            $scope.UpdateInformation();
           });
+        }
       }
     });
   }
+
+
   $scope.settings = [
     { name: 'Customize Summary', extraScreen: 'Wi-fi menu', icon: 'subject', enabled: true },
     { name: 'Customize Projects', extraScreen: 'Bluetooth menu', icon: 'subject', enabled: false },
@@ -88,14 +170,16 @@ app.controller('AppCtrl', function ($scope, $http, $mdSidenav, $mdDialog) {
       );
   };
   $scope.navigateTo = function (to, event) {
-    $mdDialog.show(
-      $mdDialog.alert()
-        .title('Navigating')
-        .content('Imagine being taken to ' + to)
-        .ariaLabel('Navigation demo')
-        .ok('Neat!')
-        .targetEvent(event)
-      );
+    // $mdDialog.show(
+    //   $mdDialog.alert()
+    //     .title('Navigating')
+    //     .content('Imagine being taken to ' + to)
+    //     .ariaLabel('Navigation demo')
+    //     .ok('Neat!')
+    //     .targetEvent(event)
+    //   );
+    
+    $scope.showAdvanced(event);
   };
   $scope.doSecondaryAction = function (event) {
     $mdDialog.show(
